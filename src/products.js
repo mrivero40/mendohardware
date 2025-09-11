e3e8e5b220eda84475ce2fafc4902c2b0ae24288
Matias E. Rivero <matiasrivero40@hotmail.com>
2025-09-11 13:51:18 -0300

    [FIX] Size height container-home, margin-block div warranty-container, delete products

--- src/products.js
+++ src/products.js
@@ -39,45 +39,14 @@ function searchCategory(category) {
 // LISTA DE PRODUCTOS - PRODUCTS LIST
 
 // CATEGORIA AURICULARES
-const auriGenius04S = new Products(
-    'geniusHs04S.jpg',
-    'Auricular c/mic HS-04S Genius (single jack)',
-    30.06,
-    true,
-    'auriculares',
-    false,
-    'https://mendohardware.com.ar/tienda/product/auricular-c-mic-hs-04s-genius-single-jack/',
-);
 const auriGeniusHs200c = new Products(
     'geniusHs200C.jpg',
     'Auricular c/mic HS-200C Genius (PC-doble jack)',
     13.5,
     true,
     'auriculares',
-    false,
+    true,
     'https://mendohardware.com.ar/tienda/product/auricular-c-mic-hs-200c-genius-pc-doble-jack/',
-);
-
-// CATEGORIA CONECTIVIDAD
-const usbWifiPerfonmance = new Products(
-    'usbWifi300.png',
-    'Wifi usb 300mbps 2.4Ghz Realtek 8192 Perfonmance',
-    15,
-    true,
-    'conectividad',
-    true,
-    'https://mendohardware.com.ar/tienda/product/wifi-usb-300mbps-2-4ghz-realtek8192-perfonmance/',
-);
-
-// CATEGORIA FUENTES
-const fuenteSlim600WCX = new Products(
-    'fuenteSlim600WCX.jpeg',
-    'Fuente Slim 600W sataX4 p/GabSlim CX',
-    53.09,
-    true,
-    'fuentes',
-    true,
-    'https://mendohardware.com.ar/tienda/product/fuente-slim-600w-satax4-p-gabslim-cx/',
 );
 
 // CATEGORIA MEMORIAS FLASH
@@ -87,20 +56,11 @@ const usbKingston32gbC = new Products(
     18,
     true,
     'memorias flash',
-    false,
+    true,
     'https://mendohardware.com.ar/tienda/product/pen-drive-usb-c-32gb-dt70-kingston/',
 );
 
 // CATEGORIA MOUSES
-const trustYvi = new Products(
-    'mouseTrust.png',
-    'Mouse inalámbrico Trust YVI ambidiestro',
-    18.64,
-    true,
-    'mouses',
-    true,
-    'https://mendohardware.com.ar/tienda/product/mouse-inalambrico-trust-yvi-ambidiestro/',
-);
 const mouGeniusScorpion = new Products(
     'mouseGeniusScorpion.jpg',
     'Mouse Gamer Scorpion M8-610 Genius',
@@ -139,15 +99,6 @@ const gigabyteKm6300 = new Products(
     true,
     'https://mendohardware.com.ar/tienda/product/tecmou-usb-ingles-gigabyte-km6300/',
 );
-const tecladoNumGenius110 = new Products(
-    'tecladoNumPadGenius110.jpg',
-    'Teclado Numeral Usb NumPad110 Genius',
-    20.86,
-    true,
-    'teclados',
-    true,
-    'https://mendohardware.com.ar/tienda/product/teclado-numeral-usb-numpad110-genius/',
-);
 
 // CATEGORIA TONER
 const tonerAlterBrotherTn580 = new Products(
@@ -170,33 +121,6 @@ const tonerAlterHp255A = new Products(
 );
 
 // CATEGORIA VARIOS
-const powerBank2600TRV = new Products(
-    'powerBank2600mahTRV.jpg',
-    'Cargador Portátil 2600mah TRV',
-    16.94,
-    true,
-    'varios',
-    true,
-    'https://mendohardware.com.ar/tienda/product/cargador-portatil-2600mah-trv/',
-);
-const cargadorInalambricoT1000 = new Products(
-    'CargadorInalambricoPerformanceT1000.jpeg',
-    'Cargador Inalámbrico T1000 Perfonmance',
-    22.27,
-    true,
-    'varios',
-    false,
-    'https://mendohardware.com.ar/tienda/product/cargador-inalambrico-t1000-perfonmance/',
-);
-const cableDisplayPortNisuta = new Products(
-    'cableDisplayPortNisuta.webp',
-    'Cable DisplayPort a HDMI 1.8m NS-CADPHD Nisuta',
-    48.1,
-    true,
-    'varios',
-    false,
-    'https://mendohardware.com.ar/tienda',
-);
 const zapatillaTRV5x220 = new Products (
     'prolongadorTRV5x220.webp',
     'Prolongador Múltiple TRV 5x220v 1.5mts switch',
